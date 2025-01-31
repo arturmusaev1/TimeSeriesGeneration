24950b66e14cfe5540383d4af3df5bb06aff4443
evildre <musaew97@gmail.com>
2025-01-31 23:14:07 +0300

    some fixes

--- code/setup_workspace.m
+++ code/setup_workspace.m
@@ -1,6 +1,6 @@
 trend = trend_highlighting(Calm, 3);
-repeated_trend = repmat(trend, 1, 1);
-data = adding_noise(repeated_trend, 'pink');
+repeated_trend = repmat(trend, 1, 3);
+data = adding_noise(repeated_trend, 'white');
 figure;
 plot(data, 'b', 'LineWidth', 2); hold on
 plot(repeated_trend, 'r', 'LineWidth', 2); 
