fcad633bcfd52e822d9281c9ea84cc86e8db6a19
evildre <musaew97@gmail.com>
2025-03-04 15:29:35 +0300

    add user anomaly

--- code/add_impulse.m
+++ code/add_impulse.m
@@ -1,20 +1,36 @@
-function trend_with_anomaly = add_impulse(trend, anomaly_type, start_idx, duration, amplitude)
+function trend_with_anomaly = add_impulse(trend, anomaly_type, start_idx, duration, amplitude, user_anomaly)
 % add_impulse Добавляет аномалию в данные.
 %
 % Входные аргументы:
-%   trend - выделенный ранее тренд данных
-%   anomaly_type - тип аномалии. Доступны 'gaussian' - аномалия,
-%   моделированная по Гауссу, 'triangle' - треугольный импульс
-%   start_idx - индекс начала аномалии
-%   duration - длительность аномалии
-%   amplitude - амлитуда аномаллии
+%   trend         - выделенный ранее тренд данных
+%   anomaly_type  - тип аномалии. Доступны 'gaussian' - аномалия,
+%                   моделированная по Гауссу, 'triangle' - треугольный импульс,
+%                   'user' - пользовательская аномалия (при этом длительность вычисляется автоматически)
+%   start_idx     - индекс начала аномалии
+%   duration      - длительность аномалии (используется для 'gaussian' и 'triangle')
+%   amplitude     - амплитуда аномалии (для масштабирования, не применяется для 'user')
+%   user_anomaly  - (необязательный) вектор, задающий пользовательскую аномалию
+%
 % Выходные аргументы:
 %   trend_with_anomaly - измененный тренд
+%
+% Если для типа 'user' параметр user_anomaly не передан, функция пытается использовать
+% глобальную переменную user_anomaly_data.
+
+    if nargin < 6 || isempty(user_anomaly)
+        global user_anomaly_data;
+        user_anomaly = user_anomaly_data;
+    end
+
     trend_with_anomaly = trend;
-    n = length(trend);
     
+    if strcmp(anomaly_type, 'user')
+        duration = length(user_anomaly);
+    end
+
+    n = length(trend);
     if start_idx + duration - 1 > n
-        error('Аномалия выходит за пределы длины тренда');
+        error('Аномалия выходит за пределы тренда');
     end
     
     switch anomaly_type
@@ -23,10 +39,21 @@ function trend_with_anomaly = add_impulse(trend, anomaly_type, start_idx, durati
         case 'gaussian'
             x = linspace(-2, 2, duration);
             anomaly = amplitude * exp(-x.^2);
+        case 'user'
+            if isempty(user_anomaly)
+                error('Пользовательская аномалия не загружена');
+            end
+            % Для пользовательской аномалии не масштабируем сигнал
+            anomaly = user_anomaly;
+            anomaly = reshape(anomaly, 1, []);
         otherwise
             error('Неизвестный тип аномалии');
     end
-    
+
+    if size(anomaly,1) > 1
+        anomaly = anomaly';
+    end
+
     trend_with_anomaly(start_idx:start_idx+duration-1) = ...
         trend_with_anomaly(start_idx:start_idx+duration-1) + anomaly;
 end
--- code/setup_workspace.m
+++ code/setup_workspace.m
@@ -1,72 +1,114 @@
 function trend_analysis_gui
-    fig = figure('Name', 'Анализ трендов', 'NumberTitle', 'off', 'Position', [100 100 900 700]);
-
-    % Кнопка загрузки данных
-    uicontrol('Style', 'pushbutton', 'String', 'Загрузить данные', 'Position', [20 650 150 30], ...
-              'Callback', @load_data);
-
-    % Поле для ввода количества повторов
-    uicontrol('Style', 'text', 'String', 'Повторы тренда:', 'Position', [200 650 100 20]);
-    trend_repeat_edit = uicontrol('Style', 'edit', 'Position', [310 650 50 25], 'String', '5');
-
-    % Кнопка добавления аномалий
-    uicontrol('Style', 'pushbutton', 'String', 'Добавить аномалию', 'Position', [20 600 150 30], ...
-              'Callback', @add_anomaly);
-
-    % Поля для ввода параметров аномалий
-    uicontrol('Style', 'text', 'String', 'Тип аномалии:', 'Position', [200 600 100 20]);
-    anomaly_type_menu = uicontrol('Style', 'popupmenu', 'Position', [310 600 100 25], ...
-                                  'String', {'gaussian', 'triangle'});
-    % Поле задания длительности аномалии
-    uicontrol('Style', 'text', 'String', 'Длительность:', 'Position', [200 570 100 20]);
-    anomaly_duration_edit = uicontrol('Style', 'edit', 'Position', [310 570 50 25], 'String', '300');
-    % Поле для ввода аномалии
-    uicontrol('Style', 'text', 'String', 'Начало:', 'Position', [200 540 100 20]);
-    anomaly_start_edit = uicontrol('Style', 'edit', 'Position', [310 540 50 25], 'String', '200');
-    % Поле задания амплитуды аномалии
-    uicontrol('Style', 'text', 'String', 'Амплитуда:', 'Position', [200 510 100 20]);
-    anomaly_amplitude_edit = uicontrol('Style', 'edit', 'Position', [310 510 50 25], 'String', '2');
-    % Поле выбора типа шума
-    uicontrol('Style', 'text', 'String', 'Тип шума:', 'Position', [200 470 100 20]);
-    noise_type_menu = uicontrol('Style', 'popupmenu', 'Position', [310 470 100 25], ...
-                                'String', {'white', 'pink'});
-    % Поле для ввода SNR
-    uicontrol('Style', 'text', 'String', 'SNR:', 'Position', [200 440 100 20]);
-    snr_edit = uicontrol('Style', 'edit', 'Position', [310 440 50 25], 'String', '2');
+    fig = figure('Name', 'Анализ трендов', 'NumberTitle', 'off', 'Position', [100 100 1100 700]);
     
-    uicontrol('Style', 'text', 'String', 'Тип аномалии: длительность, начало, амплитуда', ...
-          'Position', [650 605 200 27], 'FontWeight', 'bold', 'HorizontalAlignment', 'left');
-
-    % Поле для отображения длины тренда
-    trend_length_text = uicontrol('Style', 'text', 'String', 'Длина тренда: -', 'Position', [20 420 200 20]);
-
+    %% Левая панель управления (ширина примерно 300 пикселей)
+    % Кнопка загрузки данных
+    uicontrol('Parent', fig, 'Style', 'pushbutton', 'String', 'Загрузить данные', ...
+              'Position', [20 630 260 30], 'Callback', @load_data);
+    
+    % Повторы тренда
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Повторы тренда:', ...
+              'Position', [20 590 100 20], 'HorizontalAlignment', 'left');
+    trend_repeat_edit = uicontrol('Parent', fig, 'Style', 'edit', ...
+              'Position', [130 590 150 25], 'String', '5');
+    
+    % Выбор типа шума
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Тип шума:', ...
+              'Position', [20 555 100 20], 'HorizontalAlignment', 'left');
+    noise_type_menu = uicontrol('Parent', fig, 'Style', 'popupmenu', ...
+              'Position', [130 555 150 25], 'String', {'white', 'pink'});
+    
+    % Поле для ввода SNR
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'SNR:', ...
+              'Position', [20 520 100 20], 'HorizontalAlignment', 'left');
+    snr_edit = uicontrol('Parent', fig, 'Style', 'edit', ...
+              'Position', [130 520 150 25], 'String', '2');
+    
+    % Разделитель для параметров аномалии
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Параметры аномалии:', ...
+              'Position', [20 485 260 25], 'FontWeight', 'bold', 'HorizontalAlignment', 'left');
+    
+    % Выбор типа аномалии
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Тип аномалии:', ...
+              'Position', [20 450 100 20], 'HorizontalAlignment', 'left');
+    anomaly_type_menu = uicontrol('Parent', fig, 'Style', 'popupmenu', ...
+              'Position', [130 450 150 25], 'String', {'gaussian', 'triangle', 'user'}, 'Callback', @update_anomaly_ui);
+    
+    % Поля для длительности и амплитуды (видны для gaussian и triangle)
+    duration_label = uicontrol('Parent', fig, 'Style', 'text', 'String', 'Длительность:', ...
+              'Position', [20 415 100 20], 'HorizontalAlignment', 'left');
+    anomaly_duration_edit = uicontrol('Parent', fig, 'Style', 'edit', ...
+              'Position', [130 415 150 25], 'String', '300');
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Начало:', ...
+              'Position', [20 380 100 20], 'HorizontalAlignment', 'left');
+    anomaly_start_edit = uicontrol('Parent', fig, 'Style', 'edit', ...
+              'Position', [130 380 150 25], 'String', '200');
+    amplitude_label = uicontrol('Parent', fig, 'Style', 'text', 'String', 'Амплитуда:', ...
+              'Position', [20 345 100 20], 'HorizontalAlignment', 'left');
+    anomaly_amplitude_edit = uicontrol('Parent', fig, 'Style', 'edit', ...
+              'Position', [130 345 150 25], 'String', '2');
+    
+    % Кнопка загрузки пользовательской аномалии (видна только если выбран тип "user")
+    user_anomaly_button = uicontrol('Parent', fig, 'Style', 'pushbutton', 'String', 'Загрузить пользовательскую аномалию', ...
+              'Position', [20 415 260 30], 'Callback', @load_user_anomaly, 'Visible', 'off');
+    
+    % Кнопка добавления аномалии
+    uicontrol('Parent', fig, 'Style', 'pushbutton', 'String', 'Добавить аномалию', ...
+              'Position', [20 300 260 30], 'Callback', @add_anomaly);
+    
+    % Список добавленных аномалий
+    uicontrol('Parent', fig, 'Style', 'text', 'String', 'Список аномалий:', ...
+              'Position', [20 265 260 20], 'HorizontalAlignment', 'left');
+    anomaly_list = uicontrol('Parent', fig, 'Style', 'listbox', ...
+              'Position', [20 150 260 110]);
+    
+    % Кнопка удаления аномалии
+    uicontrol('Parent', fig, 'Style', 'pushbutton', 'String', 'Удалить аномалию', ...
+              'Position', [20 110 260 30], 'Callback', @remove_anomaly);
+    
     % Кнопка генерации данных
-    uicontrol('Style', 'pushbutton', 'String', 'Генерировать', 'Position', [20 450 150 30], ...
-              'Callback', @generate_data);
-
-    % Окно для графика (увеличенное)
-    axes_handle = axes('Units', 'pixels', 'Position', [50 100 800 300]);
-
-    % Поле для отображения списка аномалий
-    anomaly_list = uicontrol('Style', 'listbox', 'Position', [650 450 200 150]);
-
-    % Кнопка удаления аномалий
-    uicontrol('Style', 'pushbutton', 'String', 'Удалить аномалию', 'Position', [20 550 150 30], ...
-              'Callback', @remove_anomaly);
-
-    % Глобальные переменные
-    global Data anomalies;
+    uicontrol('Parent', fig, 'Style', 'pushbutton', 'String', 'Генерировать', ...
+              'Position', [20 70 260 30], 'Callback', @generate_data);
+    
+    % Отображение длины тренда
+    trend_length_text = uicontrol('Parent', fig, 'Style', 'text', 'String', 'Длина тренда: -', ...
+              'Position', [20 30 260 20], 'HorizontalAlignment', 'left');
+    
+    %% Правая часть окна - область графика
+    axes_handle = axes('Parent', fig, 'Units', 'pixels', 'Position', [350 50 720 600]);
+    
+    %% Глобальные переменные
+    global Data anomalies user_anomaly_data file_ext;
     Data = [];
     anomalies = {};
+    user_anomaly_data = [];
     file_ext = '';
-    trend_length = 0;
-    function load_data(~, ~)
-        [file, path] = uigetfile({'*.mat;*.csv', 'Файлы данных (*.mat, *.csv)'; '*.mat', 'MAT-файлы (*.mat)'; '*.csv', 'CSV-файлы (*.csv)'}, 'Выберите файл с данными');
     
+    %% Callback-функции
+    function update_anomaly_ui(~, ~)
+        type_idx = get(anomaly_type_menu, 'Value');
+        type_list = get(anomaly_type_menu, 'String');
+        anomaly_type = type_list{type_idx};
+        if strcmp(anomaly_type, 'user')
+            set(duration_label, 'Visible', 'off');
+            set(anomaly_duration_edit, 'Visible', 'off');
+            set(amplitude_label, 'Visible', 'off');
+            set(anomaly_amplitude_edit, 'Visible', 'off');
+            set(user_anomaly_button, 'Visible', 'on');
+        else
+            set(duration_label, 'Visible', 'on');
+            set(anomaly_duration_edit, 'Visible', 'on');
+            set(amplitude_label, 'Visible', 'on');
+            set(anomaly_amplitude_edit, 'Visible', 'on');
+            set(user_anomaly_button, 'Visible', 'off');
+        end
+    end
+
+    function load_data(~, ~)
+        [file, path] = uigetfile({'*.mat;*.csv','Файлы данных (*.mat, *.csv)'; '*.mat','MAT-файлы (*.mat)'; '*.csv','CSV-файлы (*.csv)'}, 'Выберите файл с данными');
         if file
-            [~, ~, ext] = fileparts(file); % Определяем расширение файла
+            [~, ~, ext] = fileparts(file);
             file_ext = ext;
-            
             if strcmp(ext, '.mat')
                 data = load(fullfile(path, file));
                 if isfield(data, 'Data')
@@ -81,30 +123,63 @@ function trend_analysis_gui
                 errordlg('Выбран неподдерживаемый формат файла!', 'Ошибка');
                 return;
             end
-            trend_length = length(Data);
-            set(trend_length_text, 'String', sprintf('Длина тренда: %d', trend_length));
+            set(trend_length_text, 'String', sprintf('Длина тренда: %d', length(Data)));
             msgbox('Данные загружены!');
         end
     end
-    function add_anomaly(~, ~)
-        if isempty(get(anomaly_duration_edit, 'String')) || isempty(get(anomaly_start_edit, 'String'))
-            msgbox('Ошибка: Поля "Длительность" и "Середина" не могут быть пустыми!', 'Ошибка', 'error');
-            return;
+
+    function load_user_anomaly(~, ~)
+        [file, path] = uigetfile({'*.mat;*.csv','Файлы аномалии (*.mat, *.csv)'; '*.mat','MAT-файлы (*.mat)'; '*.csv','CSV-файлы (*.csv)'}, 'Выберите файл с пользовательской аномалией');
+        if file
+            [~, ~, ext] = fileparts(file);
+            if strcmp(ext, '.mat')
+                anomaly_data = load(fullfile(path, file));
+                if isfield(anomaly_data, 'user_anomaly_data')
+                    user_anomaly_data = anomaly_data.user_anomaly_data;
+                else
+                    errordlg('Файл MAT не содержит переменной "user_anomaly_data"', 'Ошибка');
+                    return;
+                end
+            elseif strcmp(ext, '.csv')
+                user_anomaly_data = readmatrix(fullfile(path, file));
+            else
+                errordlg('Выбран неподдерживаемый формат файла!', 'Ошибка');
+                return;
+            end
+            msgbox('Пользовательская аномалия загружена!');
         end
+    end
+
+    function add_anomaly(~, ~)
         type_idx = get(anomaly_type_menu, 'Value');
         type_list = get(anomaly_type_menu, 'String');
         anomaly_type = type_list{type_idx};
-        duration = str2double(get(anomaly_duration_edit, 'String'));
-        amplitude = str2double(get(anomaly_amplitude_edit, 'String'));
-        start = str2double(get(anomaly_start_edit, 'String'));
-        anomalies{end + 1} = {anomaly_type, duration, start, amplitude};
+        if strcmp(anomaly_type, 'user')
+            if isempty(user_anomaly_data)
+                msgbox('Ошибка: Сначала загрузите пользовательскую аномалию!', 'Ошибка', 'error');
+                return;
+            end
+            start = str2double(get(anomaly_start_edit, 'String'));
+            anomalies{end+1} = {anomaly_type, [], start, []};
+        else
+            if isempty(get(anomaly_duration_edit, 'String')) || isempty(get(anomaly_start_edit, 'String')) || isempty(get(anomaly_amplitude_edit, 'String'))
+                msgbox('Ошибка: Поля "Длительность", "Начало" и "Амплитуда" не могут быть пустыми!', 'Ошибка', 'error');
+                return;
+            end
+            duration = str2double(get(anomaly_duration_edit, 'String'));
+            start = str2double(get(anomaly_start_edit, 'String'));
+            amplitude = str2double(get(anomaly_amplitude_edit, 'String'));
+            anomalies{end+1} = {anomaly_type, duration, start, amplitude};
+        end
         update_anomaly_list();
     end
 
     function update_anomaly_list()
-        anomaly_strings = cellfun(@(a) sprintf('%s: %d, %d, %d', a{1}, a{2}, a{3}, a{4}), anomalies, 'UniformOutput', false);
+        anomaly_strings = cellfun(@(a) sprintf('%s: дл=%s, нач=%s, ампл=%s', ...
+            a{1}, num2str(a{2}), num2str(a{3}), num2str(a{4})), anomalies, 'UniformOutput', false);
         set(anomaly_list, 'String', anomaly_strings);
     end
+
     function remove_anomaly(~, ~)
         selected_index = get(anomaly_list, 'Value');
         if selected_index > 0 && selected_index <= length(anomalies)
@@ -131,45 +206,52 @@ function trend_analysis_gui
         trend_repeats = str2double(get(trend_repeat_edit, 'String'));
         repeated_trend = repmat(trend, 1, trend_repeats);
         spoiled_trend = repeated_trend;
-
+    
         for i = 1:length(anomalies)
             anomaly = anomalies{i};
-            if anomaly{3} + anomaly{2} > length(repeated_trend)
-                msgbox('Ошибка: Аномалия выходит за пределы тренда!', 'Ошибка', 'error');
-                return;
+            if strcmp(anomaly{1}, 'user')
+                user_dur = length(user_anomaly_data);
+                if anomaly{3} + user_dur - 1 > length(repeated_trend)
+                    msgbox('Ошибка: Аномалия выходит за пределы тренда!', 'Ошибка', 'error');
+                    return;
+                end
+                spoiled_trend = add_impulse(spoiled_trend, anomaly{1}, anomaly{3}, user_dur, NaN, user_anomaly_data);
+            else
+                if anomaly{3} + anomaly{2} - 1 > length(repeated_trend)
+                    msgbox('Ошибка: Аномалия выходит за пределы тренда!', 'Ошибка', 'error');
+                    return;
+                end
+                spoiled_trend = add_impulse(spoiled_trend, anomaly{1}, anomaly{3}, anomaly{2}, anomaly{4});
             end
-            spoiled_trend = add_impulse(spoiled_trend, anomaly{1}, anomaly{3}, anomaly{2}, anomaly{4});
         end
-
+    
         rms_signal = sum((mean(median_values(:)) - median_values(:)).^2) / length(median_values);
         snr = str2double(get(snr_edit, 'String'));
         noise_type_idx = get(noise_type_menu, 'Value');
         noise_types = get(noise_type_menu, 'String');
         noise_type = noise_types{noise_type_idx};
         data = adding_noise(spoiled_trend, noise_type, rms_signal, snr);
-
+    
         if ~exist('data', 'dir')
             mkdir('data');
         end
-
+    
         num_segments = floor(length(data) / trend_length);
         if num_segments == 0
             msgbox('Недостаточно данных для разбиения на тренды!');
             return;
         end
-
+    
         trend_matrix = zeros(num_segments, trend_length);
         for i = 1:num_segments
             trend_matrix(i, :) = data((i - 1) * trend_length + 1 : i * trend_length);
         end
-
+    
         if strcmp(file_ext, '.mat')
             save('generated_data.mat', 'trend_matrix');
         elseif strcmp(file_ext, '.csv')
             writematrix(trend_matrix, 'generated_data.csv');
         end
-        
-        msgbox('Данные сохранены в файл!');
         axes(axes_handle);
         cla;
         hold on;
@@ -185,10 +267,8 @@ function trend_analysis_gui
         xlabel('Номер столбца');
         ylabel('Значение тренда');
         title('Сравнение оригинальных и сгенерированных данных');
-        legend({'Исходные данные', 'Исходный тренд', 'Данные с шумом', 'Тренд с аномалиями', 'Повторённый тренд'}, ...
-            'Location', 'best');
+        legend({'Исходные данные', 'Исходный тренд', 'Данные с шумом', 'Тренд с аномалиями', 'Повторённый тренд'}, 'Location', 'best');
         grid on;
         hold off;
-
     end
 end
